7094fe32a052fe068e973c10e9db17fb5f55a296
Gfrim <gfrim79@gmail.com>
2022-08-08 05:15:04 -0700

    Header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+int _printf(const char *format, ...);
+int print_string(char *s);
+int print_integer(int n);
+int print_binary(int n);
+int print_octal(long int n);
+
+#endif
